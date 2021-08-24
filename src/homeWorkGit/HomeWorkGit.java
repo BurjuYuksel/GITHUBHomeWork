c4222c2b89f07c3330ee83970fe38cbda82fa7f6
BurjuYuksel <burcuyuksel.t@gmail.com>
2021-08-24 14:06:39 -0400

    the third commit

--- src/homeWorkGit/HomeWorkGit.java
+++ src/homeWorkGit/HomeWorkGit.java
@@ -7,9 +7,15 @@ public class HomeWorkGit {
 		
 		
 		
-		for(int i=1;i<101;i++) {
+		for(int i=1;i<=100;i++) {
 
-			System.out.print(i+" ");
+		if (i%2!=0) {
+			
+			System.out.print(i + " ");
+			
+			
+			
+		}
 		
 
 			
