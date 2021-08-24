cea99157e9936aa346cf31e32bbcfaf9a01a0b7c
BurjuYuksel <burcuyuksel.t@gmail.com>
2021-08-24 14:00:32 -0400

    the first commit

---
+++ src/homeWorkGit/HomeWorkGit.java
@@ -0,0 +1,19 @@
+package homeWorkGit;
+
+public class HomeWorkGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		for(int i=1;i<11;i++) {
+
+			System.out.print(i+" ");
+		
+
+		}	
+		
+	}
+
+}
